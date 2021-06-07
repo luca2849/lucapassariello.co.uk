5c710e7f815cbfb3e6309d225a29b0310dc37d6e
Luca Passariello <luca@lucapassariello.co.uk>
2021-06-07 23:09:33 +0100

    Added skills, fixed responsiveness

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -2,6 +2,9 @@ import React from "react";
 
 import styles from "./Home.module.scss";
 
+import { BiServer, BiNetworkChart } from "react-icons/bi";
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+
 const Home = () => {
 	return (
 		<div className={styles.container}>
@@ -39,9 +42,98 @@ const Home = () => {
 			</section>
 			<section className={styles.section}>
 				<div className={styles.skills}>
-					<div className={styles.skill}></div>
-					<div className={styles.skill}></div>
-					<div className={styles.skill}></div>
+					<div className={styles.skill}>
+						<div className={styles.skillHeader}>
+							<div className={styles.icon}>
+								<HiOutlineDesktopComputer />
+							</div>
+							<h2>Front-End Development</h2>
+						</div>
+						<div className={styles.heading}>
+							<p>Skills</p>
+						</div>
+						<div className={styles.list}>
+							<ul>
+								<li>HTML5</li>
+								<li>CSS3</li>
+								<li>JavaScript</li>
+								<li>React</li>
+								<li>Pug (NodeJS)</li>
+								<li>Jinja (Python)</li>
+								<li>Twig (PHP)</li>
+							</ul>
+						</div>
+						<div className={styles.heading}>
+							<p>My Tools</p>
+						</div>
+						<div className={styles.list}>
+							<ul>
+								<li>VSCode</li>
+								<li>Google Chrome</li>
+								<li>Mozilla FireFox</li>
+							</ul>
+						</div>
+					</div>
+					<div className={styles.skill}>
+						<div className={styles.skillHeader}>
+							<div className={styles.icon}>
+								<BiServer />
+							</div>
+							<h2>Back-End Development</h2>
+						</div>
+						<div className={styles.heading}>
+							<p>Skills</p>
+						</div>
+						<div className={styles.list}>
+							<ul>
+								<li>NodeJS</li>
+								<li>Python</li>
+								<li>PHP</li>
+								<li>MySQL</li>
+								<li>MongoDB</li>
+								<li>Neo4J</li>
+								<li>Socket-IO / WebSockets</li>
+							</ul>
+						</div>
+						<div className={styles.heading}>
+							<p>My Tools</p>
+						</div>
+						<div className={styles.list}>
+							<ul>
+								<li>VSCode</li>
+								<li>SQL Workbench</li>
+								<li>MongoDB Compass</li>
+							</ul>
+						</div>
+					</div>
+					<div className={styles.skill}>
+						<div className={styles.skillHeader}>
+							<div className={styles.icon}>
+								<BiNetworkChart />
+							</div>
+							<h2>Networking</h2>
+						</div>
+						<div className={styles.heading}>
+							<p>Skills</p>
+						</div>
+						<div className={styles.list}>
+							<ul>
+								<li>Apache / Nginx</li>
+								<li>Virtual Hosts</li>
+								<li>Subdomains</li>
+								<li>Network Routing / Traffic</li>
+							</ul>
+						</div>
+						<div className={styles.heading}>
+							<p>My Tools</p>
+						</div>
+						<div className={styles.list}>
+							<ul>
+								<li>MobaXterm</li>
+								<li>PuTTY</li>
+							</ul>
+						</div>
+					</div>
 				</div>
 			</section>
 		</div>
